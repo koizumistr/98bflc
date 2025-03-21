270f47f0b351f54f86338539e2ba3568ff7f7680
KOIZUMI Satoru <koizumistr@minos.ocn.ne.jp>
2025-03-21 21:44:13 +0900

    more debug output

--- beta2png.c
+++ beta2png.c
@@ -5,31 +5,33 @@
 
 int main(int argc, char **argv)
 {
-    int err;
+    int err, r;
     betainfo *beta;
 
     if ((err = write_png_init()) != 0) {
-	fprintf(stderr, "libpng not found?: %d\n", err);
-	return -1;
+        fprintf(stderr, "libpng not found?: %d\n", err);
+        return -1;
     }
 
     if (argc != 2) {
-	fprintf(stderr, "usage: beta2png filename\n");
-	return -2;
+        fprintf(stderr, "usage: beta2png filename\n");
+        return -2;
     }
 
     beta = bi_make(argv[1], strlen(argv[1]), &err);
     if (beta == NULL) {
-	printf("err: %d\n", err);
-	return -1;
+        fprintf(stderr, "bi_make failed: %d\n", err);
+        return -3;
     }
     if (beta->finfo != BETA_FI_NONE) {
-	printf("finfo: %d err: %d\n", beta->finfo, err);
+        fprintf(stderr, "finfo: %d err: %d\n", beta->finfo, err);
     }
 
-    write_png(strcat(argv[1], ".png"), beta->raw, beta->current_palette);
+    r = write_png(strcat(argv[1], ".png"), beta->raw, beta->current_palette);
+    if (r != 0) {
+        fprintf(stderr, "write_png failed: %d\n", r);
+    }
     bi_dispose(beta);
-    printf("err: %d\n", err);
 
     return 0;
 }
